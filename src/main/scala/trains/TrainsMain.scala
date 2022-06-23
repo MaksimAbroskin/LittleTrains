8166f069ad243c505c1601c959207198747031b6
maksim.abroskin <Maksim.Abroskin@nordigy.ru>
2022-06-23 15:01:24 +0300

    Added tests for trainToStationsMap method

--- src/main/scala/trains/TrainsMain.scala
+++ src/main/scala/trains/TrainsMain.scala
@@ -1,17 +1,18 @@
 package trains
 
 object TrainsMain {
-  implicit val roadsMatrix: Array[Array[Int]] = Array.empty // Filled
+  implicit val roadsMatrix: Array[Array[Int]] = Array.empty
   val trains: List[Train] = Nil
 
   def trainToStationsMap(t: Train)(implicit roadsMatrix: Array[Array[Int]]): Map[(Station, TimeStamp), List[Train]] = {
     @annotation.tailrec
     def help(s: List[Station], timestamp: TimeStamp, acc: Map[(Station, TimeStamp), List[Train]]): Map[(Station, TimeStamp), List[Train]] = {
       s match {
-        case Nil | _ :: Nil => acc
+        case Nil => acc
+        case s :: Nil => acc.updated((s, timestamp), acc.getOrElse((s, timestamp), Nil) :+ t)
         case cur :: next :: tail =>
           val trainsListNow = acc.getOrElse((cur, timestamp), Nil)
-          help(next :: tail, timestamp + (roadsMatrix(cur.id)(next.id) / t.speed), acc.updated((cur, timestamp), trainsListNow :+ t))
+          help(next :: tail, timestamp + (roadsMatrix(cur.id - 1)(next.id - 1) / t.speed), acc.updated((cur, timestamp), trainsListNow :+ t))
       }
     }
     help(t.route, 0, Map.empty)
@@ -20,6 +21,6 @@ object TrainsMain {
   def mergeTwoMaps(a: Map[(Station, TimeStamp), List[Train]], b: Map[(Station, TimeStamp), List[Train]]): Map[(Station, TimeStamp), List[Train]] =
     (a.keySet ++ b.keySet).map(k => k -> a.getOrElse(k, Nil).++(b.getOrElse(k, Nil))).toMap
 
-  val map: Map[(Station, TimeStamp), List[Train]] = trains.map(trainToStationsMap).foldLeft(Map.empty[(Station, TimeStamp), List[Train]])((a, b) => mergeTwoMaps(a, b))
+  val map: Map[(Station, TimeStamp), List[Train]] = trains.map(trainToStationsMap).foldLeft(Map.empty[(Station, TimeStamp), List[Train]])(mergeTwoMaps)
 
 }
--- src/test/scala/trains/TrainsMainTest.scala
+++ src/test/scala/trains/TrainsMainTest.scala
@@ -2,9 +2,47 @@ package trains
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import TrainsMain.trainToStationsMap
 
 class TrainsMainTest extends AnyFlatSpec with Matchers {
-  "" should "" in {
+  implicit val roadsMatrix: Array[Array[Int]] = Array(
+    Array(0 , 3 , 7 , 2),
+    Array(3 , 0 , 5 , 4),
+    Array(7 , 5 , 0 , 2),
+    Array(2 , 4 , 2 , 0)
+  )
 
+  private val s1 = Station(1, 1)
+  private val s2 = Station(2, 1)
+  private val s3 = Station(3, 1)
+  private val s4 = Station(4, 1)
+
+  "trainToStationsMap method" should "work correctly for empty route" in {
+    val train = Train(1, List())
+    trainToStationsMap(train) shouldBe Map.empty
+  }
+
+  "" should "work correctly for only one station" in {
+    val train = Train(1, List(s2))
+    trainToStationsMap(train) shouldBe Map((s2, 0) -> List(train))
+  }
+
+  "" should "work correctly for repeated station in route" in {
+    val train = Train(1, List(s2, s3, s2))
+    trainToStationsMap(train) shouldBe Map(
+      (s2, 0) -> List(train),
+      (s3, 5) -> List(train),
+      (s2, 10) -> List(train)
+    )
+  }
+
+  "" should "convert Train to stations map" in {
+    val train = Train(1, List(s1, s4, s3, s2))
+    trainToStationsMap(train) shouldBe Map(
+      (s1, 0) -> List(train),
+      (s4, 2) -> List(train),
+      (s3, 4) -> List(train),
+      (s2, 9) -> List(train),
+    )
   }
 }
